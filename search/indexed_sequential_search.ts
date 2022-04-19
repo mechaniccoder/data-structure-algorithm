90f87a8a020ebe0d6efd6e2e2f58db5aec6582f3
mechaniccoder <yuseunghwan94@gmail.com>
2022-04-19 23:36:56 +0900

    Index sequential search - documentation

--- search/indexed_sequential_search.ts
+++ search/indexed_sequential_search.ts
@@ -4,6 +4,12 @@ const indexTable = [
   { key: 67, index: 6 },
 ];
 
+/**
+ * main data를 가지고 있는 테이블에서 데이터를 가져와서 인덱스와 데이터정보를 가지고 있는 index table을 생성한다.
+ * 그럼 찾고자 하는 데이터가 어느 인덱스 사이에 있는지를 알 수 있고, 그 사이에 대해서만 binary search 하면 되기때문에
+ * 더 빠르게 검색을 할 수 있다.
+ * 데이터베이스 여러 분야에서 사용 된다.
+ */
 function searchIndex(key: number, n: number) {
   if (key < indexTable[0].key || indexTable[indexTable.length - 1].key < key)
     return -1;
