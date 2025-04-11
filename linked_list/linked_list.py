640bad283472bf4c43a9f6c0f0ddfb43aac298cd
mechaniccoder <yuseunghwan94@gmail.com>
2025-04-11 23:23:43 +0900

    feat: implement delete in linked list

--- linked_list/linked_list.py
+++ linked_list/linked_list.py
@@ -25,6 +25,31 @@ class LinkedList:
 
         return self
 
+    def delete(self, value):
+        if not self.head:
+            return
+
+        deletednode = None
+
+        while self.head and self.head.value == value:
+            deletednode = self.head
+            self.head = self.head.next
+
+        current_node = self.head
+
+        if current_node:
+            while current_node.next:
+                if current_node.next.value == value:
+                    deletednode = current_node.next
+                    current_node.next = current_node.node.next.next
+                else:
+                    current_node = current_node.next
+
+        if self.tail.value == value:
+            self.tail = current_node
+
+        return deletednode
+
 
 class LinkedListNode:
     def __init__(self, value, next):
